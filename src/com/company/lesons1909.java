9305052523ef1782ede5d4243e3729f8b7b8861f
Admin <dmtr2001@mail.ru>
2022-10-01 18:53:10 +0700

    redaction 01.10.2022

---
+++ src/com/company/lesons1909.java
@@ -0,0 +1,32 @@
+package com.company;
+public class lesons1909 {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 765789;
+        System.out.println(paper);
+
+        System.out.println("Задача 2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat += 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+
+        //Задача 3
+        System.out.println("Задача 3");
+        var dog1 = 8.0;
+        dog1 = dog1 - 3.5;
+        System.out.println(dog1);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper -= 7639; // paper = paper - 7639;
+        System.out.println(paper);
+    }
+}
